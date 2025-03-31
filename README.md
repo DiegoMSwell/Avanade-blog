b18761b4a32fc037d7daa78a07efb41f75bcb6fb
Diego <diegomoises.0913@gmail.com>
2025-03-30 22:14:17 -0300

    feat: inicial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# AngularBlog
+
+Este projeto é um mini blog desenvolvido em Angular para compartilhar conteúdos sobre a Avanade. 
+
+[Angular CLI](https://github.com/angular/angular-cli) version 19.2.5.
+
+## Servidor de Desenvolvimento
+
+Execute o comando:
+
+`ng serve`
+
+Depois, acesse `http://localhost:4200/` no navegador. A aplicação será recarregada automaticamente sempre que houver alterações no código.
+
+## Gerando Componentes
+
+Para criar um novo componente, utilize: `ng generate component nome-do-componente` Também é possível gerar diretivas, pipes, serviços, classes, guards, interfaces, enums e módulos usando: `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+
+## Ajuda Adicional
+
+Para mais informações sobre o Angular CLI, use: `ng help` Ou acesse a [Documentação oficial do Angular CLI](https://angular.io/cli).
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "25 ANOS DE AVANADE",
+    "description": " Há mais de 25 anos que ajudam os clientes de todos os setores a atingir o seu potencial máximo, desbloqueando o poder da Microsoft.<br> <a style='color: blue;' href='https://www.avanade.com/pt-br/about' target='_blank'> Clique aqui para saber mais sobre a Avanade </a>",
+    "photoCover":"https://mir-s3-cdn-cf.behance.net/projects/404/6c9cb5196146257.Y3JvcCw4MDgsNjMyLDAsMA.png"
+  },
+  {
+    "id":"2",
+    "title": "Bootcamp 2025",
+    "description": " Decola Tech Avanade 2025! Sua oportunidade de estágio em uma empresa que faz a tecnologia acontecer.<br> <a style='color: blue;' href='https://www.dio.me/bootcamp/decola-tech-2025' target='_blank'> Clique aqui para saber mais sobre o Bootcamp </a>",
+    "photoCover":"https://assets.dio.me/PTXvFOaK1CWYHJxAn70yuu9vJ6CNI3ZLDy4GonX2-1I/f:webp/h:221/q:80/L3RyYWNrcy9jb3Zlci81M2VkYjJlMy04MTdlLTQwNjUtOGIwZS0zMGI5NzA4YjM4MTQucG5n"
+  },
+  {
+    "id":"3",
+    "title": "Bootcamp 2024",
+    "description": " Decola Tech Avanade 2024! Sua oportunidade de estágio em uma empresa que faz a tecnologia acontecer.<br> <a style='color: blue;' href='https://www.dio.me/bootcamp/decola-tech-avanade-net-developer' target='_blank'> Clique aqui para saber mais sobre o Bootcamp </a>",
+    "photoCover":"https://assets.dio.me/vf5i6n_eWTLH3y6-biYNjhXq1J8xTNrrhIfgyKKqbm0/f:webp/q:80/L2ZpbGVzL2Fzc2V0cy9hNTVmOWI4Yy01MWFmLTRjMWItYTlkNy1kMzhkMmI0ZDdlM2UucG5n"
+  },
+  {
+    "id":"4",
+    "title": "Bootcamp 2023",
+    "description": "Decola Tech Avanade 2023! Sua oportunidade de estágio em uma empresa que faz a tecnologia acontecer.<br> <a style='color: blue;' href='https://www.dio.me/bootcamp/decola-tech-3a-edicao' target='_blank'> Clique aqui para saber mais sobre o Bootcamp </a>",
+    "photoCover":"https://assets.dio.me/ZU_XCdelfnDc89KjTYCw5T6fbGUmWnSWV_f0TDKlszs/f:webp/q:80/L2ZpbGVzL2Fzc2V0cy8zMzBhZjIxYy0yNWMwLTQ3ZjUtOGFmZC01N2NmNzI2YjkxNDkucG5n"
+  }
+]
